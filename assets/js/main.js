15ef64a2d47319ea251e621141268f67a867a136
teddyvn2017 <ware4u@gmail.com>
2024-01-05 08:43:19 +0700

    update style css

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,9 @@ const showMenu = (toggleId, navId) => {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    autoplay: {
+      delay: 3000,
+    },
     // If we need pagination
     // pagination: {
     //   el: '.swiper-pagination',
